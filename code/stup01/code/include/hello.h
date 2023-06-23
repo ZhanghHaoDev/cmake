7752ad4bbbd12f214df0621d2cfcbf31c0dd4140
张浩 <29505921+Hao2233@users.noreply.github.com>
2023-06-23 11:44:50 +0800

    cmake_step01

---
+++ code/stup01/code/include/hello.h
@@ -0,0 +1,15 @@
+//
+// Created by agp13 on 2023/6/22.
+//
+
+#ifndef STUP01_HELLO_H
+#define STUP01_HELLO_H
+
+
+class hello
+{
+public:
+    int HELLO = 99999999;
+};
+
+#endif //STUP01_HELLO_H
---
+++ code/stup01/code/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "hello.h"
+
+int main()
+{
+    int num = 0;
+
+    hello h;
+    std::cout << h.HELLO << std::endl;
+    std::cout << num;
+
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
